5bafac3910978775012ac0a4b40ffb59f1723fdc
Torusrxxx <torusrxxx@users.noreply.github.com>
2023-07-22 04:18:27 +0000

    Update atoi.h

--- src/atoi.h
+++ src/atoi.h
@@ -6,17 +6,18 @@ inline int sse42_atoi(const char* str)
     __m128i limits = _mm_setr_epi8('0', '9', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     __m128i zeros = _mm_set1_epi8('0');
     __m128i d = _mm_loadu_si128((const __m128i*)str);
+    __m128i signxmm = _mm_setzero_si128();
     int sign = _mm_extract_epi8(d, 0);
+    int sign2;
     // check the sign
     if (sign == '-')
-        sign = 1;
+        sign2 = -1;
     else
-        sign = 0; //'+', ' ' (assume legal input)
-	if (sign == '+' || sign == '-' || sign == ' ')
-	{
+        sign2 = 1; //'+', ' ' (assume legal input)
+    signxmm = _mm_insert_epi32(signxmm, sign2, 0);
+    if (sign == '+' || sign == '-' || sign == ' ')
         // shift 1 byte to remove - sign
         d = _mm_srli_si128(d, 1);
-	}
     // below are the same as atoul
     // get characters within 0 to 9
     __m128i check = _mm_cmpistrm(limits, d, _SIDD_UBYTE_OPS | _SIDD_CMP_RANGES | _SIDD_UNIT_MASK);
@@ -29,11 +30,9 @@ inline int sse42_atoi(const char* str)
     d = _mm_sub_epi8(d, check);
     d = _mm_maddubs_epi16(d, _mm_set1_epi16(0x0A01));
     d = _mm_madd_epi16(d, _mm_set1_epi32(0x00640001));
-    d = _mm_mullo_epi32(d, _mm_setr_epi32(1, 10000, 1, 10000));
+    d = _mm_mullo_epi32(d, _mm_setr_epi32(1, 10000, 100000000, 0));
     d = _mm_hadd_epi32(d, d);
-    unsigned long result = _mm_extract_epi32(d, 0) + _mm_extract_epi32(d, 1) * 100000000;
-    if (sign)
-        return -(int)result;
-    else
-        return (int)result;
+    d = _mm_hadd_epi32(d, d);
+    d = _mm_sign_epi32(d, signxmm);
+    return _mm_extract_epi32(d, 0);
 }
